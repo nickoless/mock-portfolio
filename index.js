21e1fde599e7e5e16c0e52e181e554a78b603659
nickoless <nickoless.lee@gmail.com>
2020-01-21 20:48:24 -0800

    implemented node-sass-middleware

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const express = require('express');
+const express = require("express");
+const sassMiddleware = require("node-sass-middleware");
+const path = require("path");
 const app = express();
 
+const srcPath = __dirname + "/scss";
+const destPath = __dirname + "/public/css";
+
 app.set("view engine", "ejs");
-app.use("/styles", express.static(__dirname + "/styles"));
+app.use(
+  sassMiddleware({
+    src: srcPath,
+    dest: destPath,
+    debug: true,
+    outputStyle: "compressed",
+    prefix: "/css"
+  })
+);
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/scripts", express.static(__dirname + "/scripts"));
-app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("main");
-})
+});
 
 app.listen(3000, () => {
-  console.log('Ayy, we up on port 3000!')
+  console.log("Ayy, we up on port 3000!");
 });
